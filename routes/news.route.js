77d6bce319ed2b3330378d66f39d14e24dfaab22
Ralph Lauren <81021854+abuizid@users.noreply.github.com>
2022-03-14 16:41:30 +0300

    Draniki

---
+++ routes/news.route.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const { newsController } = require("../controllers/news.controller");
+
+const router = Router();
+
+router.post("/news", newsController.addNews)
+router.delete("/news/:id", newsController.deleteNews)
+router.get('/news', newsController.getNews)
+router.get('/news/:id', newsController.getNewsById) //вывод определенной новости по АйДи
+router.get('/news/category/:id', newsController.getNewsByCategory) //вывод всех новостей по категории
+
+module.exports = router;
+

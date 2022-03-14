d0a4e2ad9de08e385634760237f1ae8ba243af2b
Ralph Lauren <81021854+abuizid@users.noreply.github.com>
2022-03-14 16:48:47 +0300

    Dranik23232

--- routes/news.route.js
+++ routes/news.route.js
@@ -8,6 +8,6 @@ router.delete("/news/:id", newsController.deleteNews)
 router.get('/news', newsController.getNews)
 router.get('/news/:id', newsController.getNewsById) //вывод определенной новости по АйДи
 router.get('/news/category/:id', newsController.getNewsByCategory) //вывод всех новостей по категории
-
+//лагман
 module.exports = router;
 
